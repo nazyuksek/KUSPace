9efb964183eaf7a68fa8bb6ee9329f5e5a4bb5ed
sozdemir17 <73183223+sozdemir17@users.noreply.github.com>
2022-03-16 22:48:33 +0300

    Admin Login Screen

--- Hali/screens/AdminLoginScreen.tsx
+++ Hali/screens/AdminLoginScreen.tsx
@@ -2,27 +2,42 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import * as React from "react";
 import Login from "../components/Login";
 import TextField from "../components/TextField";
+import Button from "../components/Button";
 
-export default function AdminLoginScreen() {
+export default function AdminLoginScreen(navigation: any) {
   const [text, setText] = React.useState("");
   const [search, setSearch] = React.useState("");
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.itemscontainer}>
+        <Text style={styles.text}>HALI</Text>
+        <Text style={styles.subtext}>
+          A platform that connects football players and football pitch managers!
+        </Text>
         <Login></Login>
         <TextField
           text={"pitch id"}
-          style={{}}
+          style={{ marginTop: 110 }}
           textState={text}
           setText={setText}
           setSearch={setSearch}
         ></TextField>
+        <Button
+          onPress={() => {}}
+          buttonText="Login"
+          style={{ backgroundColor: "white", marginTop: 50 }}
+        ></Button>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    color: "white",
+    fontSize: 48,
+    fontWeight: "900",
+  },
   container: {
     display: "flex",
     flex: 1,
@@ -33,9 +48,12 @@ const styles = StyleSheet.create({
     display: "flex",
     alignItems: "center",
   },
-  text: {
+  subtext: {
+    textAlign: "center",
+    marginTop: 20,
+    fontSize: 16,
+    fontWeight: "500",
     color: "white",
-    fontSize: 48,
-    fontWeight: "900",
+    marginBottom: 20,
   },
 });

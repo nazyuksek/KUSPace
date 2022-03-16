a0d8fdd4703996ce10f0dd752a8aec2546cc54a5
Naz Yüksek <nyuksek17@ku.edu.tr>
2022-03-16 12:13:14 +0300

    navigation initialized

--- Hali/App.tsx
+++ Hali/App.tsx
@@ -6,18 +6,30 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import LandingPage from './screens/LandingPage';
+import { ScreenStack } from 'react-native-screens';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+  const Stack = createNativeStackNavigator();
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <LandingPage />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="LandingPage"
+            component={LandingPage}
+          />
+          <Stack.Screen
+            name="LandingPage"
+            component={LandingPage}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
